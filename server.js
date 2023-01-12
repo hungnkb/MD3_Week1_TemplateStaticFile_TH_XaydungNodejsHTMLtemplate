2924423e3250a5b433c254bf83d4da9cf87e25b3
unknown <hungnkb>
2023-01-12 16:36:06 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+
+let server = http.createServer((req, res) => {
+    fs.readFile('./templates/index.html','utf-8',(err, str) => {
+        if(err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end('404 Not Found');
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(str);
+        return res.end();
+    });
+
+});
+
+server.listen('8080', () => {
+    console.log(`Sever is running at localhost:8080`)
+})
